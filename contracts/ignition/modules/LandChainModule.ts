01e471e5795d82222a3a1eeea8fc428279730671
antokumar99 <antokumar101@gmail.com>
2026-07-27 03:34:15 +0600

    fix

--- contracts/ignition/modules/LandChainModule.ts
+++ contracts/ignition/modules/LandChainModule.ts
@@ -3,7 +3,8 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 export default buildModule("LandChainModule", (m) => {
   const owner = m.getAccount(0);
   const verifier = m.contract("Verifier");
-  const registry = m.contract("LandRegistry", [owner, verifier]);
+  const rootVerifier = m.contract("RootVerifier");
+  const registry = m.contract("LandRegistry", [owner, verifier, rootVerifier]);
 
-  return { verifier, registry };
+  return { verifier, rootVerifier, registry };
 });
